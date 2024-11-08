4212e66afd46be446c5946f448983ec0575c763a
Voravich S <vsilapac@gmail.com>
2024-11-07 22:43:39 -0500

    cool method bro

--- CoolClass.java
+++ CoolClass.java
@@ -2,4 +2,8 @@ public class CoolClass {
     public static void main(String[] args) {
         System.out.println("Reporting in from branch 1!");
     }
+
+    public static void coolMethod() {
+        System.out.println("nice :)");
+    }
 }
