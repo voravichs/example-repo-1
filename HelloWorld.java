6d5701981f15d09cdbcb954febca8e92e4b16c03
Voravich S <vsilapac@gmail.com>
2024-11-07 22:49:46 -0500

    conflict part 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public void conflict() {
+        System.out.println("oh no");
+    }
 }
